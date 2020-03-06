ab138b2d6da981915dc153d024588ca011aad17a
Mahoken <58619620+SekiShuhei@users.noreply.github.com>
2020-03-06 17:56:52 +0900

    メニュー開く／閉じる動作実装

--- Test/ARVirtualScreen.cpp
+++ Test/ARVirtualScreen.cpp
@@ -164,41 +164,7 @@ void ARVirtualScreen::Draw()
 
 
 	{
-
-		
-		//// キャプチャ領域指定.
-		//double x = s3d::Scene::Size().x - 300;
-		//double y = 0;
-		//double h = 30;
-		//
-		//if (SimpleGUI::Slider(U"R {:.2f}"_fmt(this->capture_region.x), 
-		//	this->capture_region.x, 0, 2000, Vec2(x, y), 100, 200))
-		//{
-		//	this->capture_region_updated = true;
-		//}
-		//y += h;
-		//if (SimpleGUI::Slider(U"R {:.2f}"_fmt(this->capture_region.y), 
-		//	this->capture_region.y, 0, 2000, Vec2(x, y), 100, 200))
-		//{
-		//	this->capture_region_updated = true;
-		//}
-		//y += h;
-		//if (SimpleGUI::Slider(U"R {:.2f}"_fmt(this->capture_region.w), 
-		//	this->capture_region.w, 200, 2000, Vec2(x, y), 100, 200))
-		//{
-		//	this->capture_region_updated = true;
-		//	this->capture_size_updated = true;
-		//}
-		//y += h;
-		//if (SimpleGUI::Slider(U"R {:.2f}"_fmt(this->capture_region.h), 
-		//	this->capture_region.h, 200, 2000, Vec2(x, y), 100, 200))
-		//{
-		//	this->capture_region_updated = true;
-		//	this->capture_size_updated = true;
-		//}
-		//y += h;
-		
-		
+		// キャプチャテクスチャの管理.
 		
 		if (this->capture_size_updated)
 		{
--- Test/GuiMenu.cpp
+++ Test/GuiMenu.cpp
@@ -13,40 +13,40 @@ namespace ARWorkspace {
 
 void GuiMenu::Draw()
 {
-	
-	//if (! this->pull_down_flag)
-	//{
-	//	// 開くボタン.
-	//	//...
-	//	this->pull_down_flag = true;
-	//	return;
-	//}
-
-	// キャプチャ領域指定.
 	double x = s3d::Scene::Size().x - 300;
 	double y = 0;
 	double h = 30;
 
-	if (SimpleGUI::Slider(U"R {:.2f}"_fmt(this->model.GetCaptureRegion().x),
+	
+	if (! this->pull_down_flag)
+	{
+		if (s3d::SimpleGUI::Button(U"▼CaptureSetting", Vec2(x,y)))
+		{
+			this->pull_down_flag = true;
+		}
+		return;
+	}
+
+	if (SimpleGUI::Slider(U"X {:.2f}"_fmt(this->model.GetCaptureRegion().x),
 		this->model.GetCaptureRegion().x, 0, 2000, Vec2(x, y), 100, 200))
 	{
 		this->model.CaptureRegionUpdate();
 	}
 	y += h;
-	if (SimpleGUI::Slider(U"R {:.2f}"_fmt(this->model.GetCaptureRegion().y),
+	if (SimpleGUI::Slider(U"Y {:.2f}"_fmt(this->model.GetCaptureRegion().y),
 		this->model.GetCaptureRegion().y, 0, 2000, Vec2(x, y), 100, 200))
 	{
 		this->model.CaptureRegionUpdate();
 	}
 	y += h;
-	if (SimpleGUI::Slider(U"R {:.2f}"_fmt(this->model.GetCaptureRegion().w),
+	if (SimpleGUI::Slider(U"Width {:.2f}"_fmt(this->model.GetCaptureRegion().w),
 		this->model.GetCaptureRegion().w, 200, 2000, Vec2(x, y), 100, 200))
 	{
 		this->model.CaptureRegionUpdate();
 		this->model.CaptureSizeUpdate();
 	}
 	y += h;
-	if (SimpleGUI::Slider(U"R {:.2f}"_fmt(this->model.GetCaptureRegion().h),
+	if (SimpleGUI::Slider(U"Height {:.2f}"_fmt(this->model.GetCaptureRegion().h),
 		this->model.GetCaptureRegion().h, 200, 2000, Vec2(x, y), 100, 200))
 	{
 		this->model.CaptureRegionUpdate();
@@ -54,8 +54,12 @@ void GuiMenu::Draw()
 	}
 	y += h;
 
+	//...
 
-
+	if (s3d::SimpleGUI::Button(U"▲Close", Vec2(x, y)))
+	{
+		this->pull_down_flag = false;
+	}
 
 }
 
