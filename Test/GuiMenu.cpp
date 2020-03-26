90bd59faefa019032ee1d38dca492316524f07a4
Mahoken <58619620+SekiShuhei@users.noreply.github.com>
2020-03-26 12:33:54 +0900

    GUI改良

--- Test/GuiMenu.cpp
+++ Test/GuiMenu.cpp
@@ -13,14 +13,17 @@ namespace ARWorkspace {
 
 void GuiMenu::Draw()
 {
-	double x = s3d::Scene::Size().x - 300;
+	int text_width = 150;
+	int slider_width = 200;
+
+	double x = s3d::Scene::Size().x - (text_width + slider_width);
 	double y = 0;
 	double h = 30;
 
 	
 	if (! this->pull_down_flag)
 	{
-		if (s3d::SimpleGUI::Button(U"¥CaptureSetting", Vec2(x,y)))
+		if (s3d::SimpleGUI::Button(U"¥CaptureSetting", Vec2(x,y), text_width + slider_width))
 		{
 			this->pull_down_flag = true;
 		}
@@ -28,26 +31,26 @@ void GuiMenu::Draw()
 	}
 
 	if (SimpleGUI::Slider(U"X {:.2f}"_fmt(this->model.GetCaptureRegion().x),
-		this->model.GetCaptureRegion().x, -2000, 2000, Vec2(x, y), 100, 200))
+		this->model.GetCaptureRegion().x, 0, 2000, Vec2(x, y), text_width, slider_width))
 	{
 		this->model.CaptureRegionUpdate();
 	}
 	y += h;
 	if (SimpleGUI::Slider(U"Y {:.2f}"_fmt(this->model.GetCaptureRegion().y),
-		this->model.GetCaptureRegion().y, -2000, 2000, Vec2(x, y), 100, 200))
+		this->model.GetCaptureRegion().y, 0, 2000, Vec2(x, y), text_width, slider_width))
 	{
 		this->model.CaptureRegionUpdate();
 	}
 	y += h;
 	if (SimpleGUI::Slider(U"Width {:.2f}"_fmt(this->model.GetCaptureRegion().w),
-		this->model.GetCaptureRegion().w, 200, 2000, Vec2(x, y), 100, 200))
+		this->model.GetCaptureRegion().w, 200, 2000, Vec2(x, y), text_width, slider_width))
 	{
 		this->model.CaptureRegionUpdate();
 		this->model.CaptureSizeUpdate();
 	}
 	y += h;
 	if (SimpleGUI::Slider(U"Height {:.2f}"_fmt(this->model.GetCaptureRegion().h),
-		this->model.GetCaptureRegion().h, 200, 2000, Vec2(x, y), 100, 200))
+		this->model.GetCaptureRegion().h, 200, 2000, Vec2(x, y), text_width, slider_width))
 	{
 		this->model.CaptureRegionUpdate();
 		this->model.CaptureSizeUpdate();
@@ -56,7 +59,7 @@ void GuiMenu::Draw()
 
 	//...
 
-	if (s3d::SimpleGUI::Button(U"£Close", Vec2(x, y)))
+	if (s3d::SimpleGUI::Button(U"£Close", Vec2(x, y), text_width + slider_width))
 	{
 		this->pull_down_flag = false;
 	}
