90656edad5b5b6de2612e03b228068147856166c
Mahoken <58619620+SekiShuhei@users.noreply.github.com>
2020-04-04 21:52:56 +0900

    ドロップダウンメニュー実装中

--- Test/DropDownMenu.cpp
+++ Test/DropDownMenu.cpp
@@ -8,15 +8,24 @@ DropDownMenu::DropDownMenu(DropDownMenuDrawEvent arg_draw_event) :
 {
 
 }
-bool DropDownMenu::Draw(s3d::Vec2 arg_position)
+bool DropDownMenu::Draw(s3d::Vec2 arg_position, int arg_width)
 {
+	this->position = arg_position;
+	this->width = arg_width;
 	if (! this->is_open)
 	{
-		// トップレベルボタンのみ表示する.
-
+		if (s3d::SimpleGUI::Button(U"▼" + this->top_label, this->position, this->width))
+		{
+			this->is_open = true;
+		}
 		return true;
 	}
 	// 一番上にも閉じるボタン配置.
+	if (s3d::SimpleGUI::Button(U"▲" + this->top_label, this->position, this->width))
+	{
+		this->is_open = false;
+	}
+	// 改行すみ高さ値を渡す必要がある.
 
 	bool result = this->draw_event();
 
--- Test/DropDownMenu.hpp
+++ Test/DropDownMenu.hpp
@@ -12,7 +12,7 @@ public:
 	//DropDownMenu() = 0;
 	DropDownMenu(DropDownMenuDrawEvent arg_draw_event);
 public:
-	bool Draw(s3d::Vec2 arg_position);
+	bool Draw(s3d::Vec2 arg_position, int arg_width);
 
 	void SetLabel(s3d::String arg_label)
 	{
@@ -22,12 +22,17 @@ public:
 	{
 		this->position = arg_position;
 	}
+	void SetWidth(int arg_width)
+	{
+		this->width = arg_width;
+	}
 
 private:
 	bool					is_open = false;
 
 	s3d::String				top_label = U"";
 	s3d::Vec2				position = Vec2(0,0);
+	int						width = 100;
 	DropDownMenuDrawEvent	draw_event = [](){return false;};
 };
 
