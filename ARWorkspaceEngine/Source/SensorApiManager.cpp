1ee0d18d5835e6df4eafb6c067eb2a5f148a0c1a
Mahoken <58619620+SekiShuhei@users.noreply.github.com>
2020-04-13 21:58:58 +0900

    コード整理

--- ARWorkspaceEngine/Source/SensorApiManager.cpp
+++ ARWorkspaceEngine/Source/SensorApiManager.cpp
@@ -81,63 +81,6 @@ std::optional<Vector3> SensorApiManager::GetGyrometerSensorData()
 
 }
 
-void SensorApiManager::GetGyrometerSensorData(double& ref_x, double& ref_y, double& ref_z)
-{
-	return; //kari.
-
-	ref_x = 0.f;
-	ref_y = 0.f;
-	ref_z = 0.f;
-
-	CComPtr<ISensorManager> p_sensor_manager;
-	CComPtr<ISensorCollection> sensor_collection;
-	CComPtr<ISensor> sensor;
-	CComPtr<ISensorDataReport> data;
-
-	//if (FAILED(::CoInitializeEx(NULL, COINIT_MULTITHREADED)))
-	//{
-	//	return;
-	//}
-	// 毎回マネージャを作成する必要もない？？.
-	if (FAILED(::CoCreateInstance(CLSID_SensorManager, NULL, CLSCTX_INPROC_SERVER, IID_PPV_ARGS(&p_sensor_manager))))
-	{
-		return;
-	}
-	// .
-	if (FAILED(p_sensor_manager->GetSensorsByCategory(SENSOR_TYPE_GYROMETER_3D, &sensor_collection)))
-	{
-		return;
-	}
-	if (FAILED(sensor_collection->GetAt(0, &sensor)))
-	{
-		return;
-	}
-	if (FAILED(sensor->GetData(&data)))
-	{
-		return;
-	}
-
-	PROPVARIANT x = {};
-	if (FAILED(data->GetSensorValue(SENSOR_DATA_TYPE_ANGULAR_VELOCITY_X_DEGREES_PER_SECOND, &x)))
-	{
-		return;
-	}
-	PROPVARIANT y = {};
-	if (FAILED(data->GetSensorValue(SENSOR_DATA_TYPE_ANGULAR_VELOCITY_Y_DEGREES_PER_SECOND, &y)))
-	{
-		return;
-	}
-	PROPVARIANT z = {};
-	if (FAILED(data->GetSensorValue(SENSOR_DATA_TYPE_ANGULAR_VELOCITY_Z_DEGREES_PER_SECOND, &z)))
-	{
-		return;
-	}
-
-	ref_x = x.dblVal;
-	ref_y = y.dblVal;
-	ref_z = z.dblVal;
-}
-
 bool SensorApiManager::selectSensor(const REFSENSOR_CATEGORY_ID arg_sensor_category_id)
 {
 	// とりあえず先頭のセンサを見つけるだけ.
--- ARWorkspaceEngine/Source/SensorApiManager.hpp
+++ ARWorkspaceEngine/Source/SensorApiManager.hpp
@@ -22,18 +22,14 @@ public:
 	std::optional<Vector3> GetAccelerometerSensorData();
 	std::optional<Vector3> GetGyrometerSensorData();
 
-	void GetGyrometerSensorData(double& ref_x, double& ref_y, double& ref_z);
-
 private:
 	bool selectSensor(const REFSENSOR_CATEGORY_ID arg_sensor_category_id); 
 	double getCurrentSensorValue(const PROPERTYKEY arg_property_key);
 
 private:
 	CComPtr<ISensorManager>		p_sensor_manager;
-	CComPtr<ISensorCollection>	p_sensor_collection;
 	CComPtr<ISensor>			p_current_sensor;
-	CComPtr<ISensorDataReport>	p_data;
-
+	
 	bool intialized = false;
 };
 
