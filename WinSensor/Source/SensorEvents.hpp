ae2c80cdca77aca1b6c99d2bcf6a63e064bbd715
Shuhei Seki <siuhe@abelia.ocn.ne.jp>
2020-04-24 23:32:23 +0900

    TODOコメント、SAL注釈更新

--- WinSensor/Source/SensorEvents.hpp
+++ WinSensor/Source/SensorEvents.hpp
@@ -9,15 +9,13 @@ namespace WinSensor {
 class SensorEvents : public ISensorEvents
 {
 public:
-	SensorEvents(SensorEventCallbackFunction callback_func);
+	SensorEvents(SensorEventCallbackFunction callback_data_updated);
 	// -------------IUnknownInterface.-------------
 	ULONG __stdcall AddRef();
 	ULONG __stdcall Release();
 	HRESULT __stdcall QueryInterface(const IID&, void**);
 private:
 	unsigned long ref_count;
-
-
 	// -------------ISensorEvents.-------------
 public:
 	HRESULT __stdcall OnEvent(__RPC__in_opt ISensor* p_sensor, REFGUID eventID, IPortableDeviceValues* p_event_data);
@@ -25,10 +23,10 @@ public:
 	HRESULT __stdcall OnLeave(__RPC__in REFSENSOR_ID sensorID);
 	HRESULT __stdcall OnStateChanged(__RPC__in_opt ISensor* p_sensor, SensorState state);
 	// ---------------------------------------
-public:
-
 private:
-	SensorEventCallbackFunction callback_func;
+	SensorEventCallbackFunction callback_data_updated;
+	//SensorEventCallbackFunction callback_sensor_leaved;
+
 };
 
 }
