6eca4f8f9dbd57613560e88a981391fc0da8fe77
Shuhei Seki <siuhe@abelia.ocn.ne.jp>
2020-05-13 08:55:01 +0900

    コード整理

--- ARWorkspaceEngine/Source/WinScreenCapture.cpp
+++ ARWorkspaceEngine/Source/WinScreenCapture.cpp
@@ -1,8 +1,6 @@
 
-
 #include <math.h>
 #include "WinScreenCapture.hpp"
-
 #pragma comment(lib, "shcore.lib")
 #include <ShellScalingApi.h>
 
@@ -100,8 +98,9 @@ bool WinScreenCapture::CaptureScreen(s3d::Image& read_image, int x, int y, int w
 	this->bmpInfo.bmiHeader.biBitCount = 32;
 	this->bmpInfo.bmiHeader.biCompression = BI_RGB;
 	this->bmpInfo.bmiHeader.biSizeImage = this->GetBitmapImageSize(this->bmpInfo);
+	
 
-	HDC screen_dc = GetDC(NULL);
+	HDC screen_dc = ::GetDC(NULL);
 	
 	HDC hPrevMemDC = this->hMemDC;
 	HBITMAP hPrevBitmap = this->hBitmap;
